b505689bb54388048c659ff18f96977da2d71850
Annebeth <anha1001@stud.ek.dk>
2025-11-06 12:37:54 +0100

    Torsdag test branch

--- instruction.js
+++ instruction.js
@@ -1,3 +1,21 @@
 instruction.js 
-console.log()
+console.log("Load instruction..")
+document.querySelector
 
+
+function Fontallap (){
+
+} 
+
+
+
+
+
+
+// function lyd(){
+//     console.log("lyd er vigtigt");
+//     document.querySelector("#lyd").classList.add("min_lyd")
+//     document.querySelector ("opskrifter").style.fill ="white"
+
+//     document.querySelector("info-text h2").textContent ="Fontallap"
+// }
